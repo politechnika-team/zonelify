767f3b0cd003ca0724b2ba05e80959138e00e38c
Taoo111 <dawidjura12@gmail.com>
2023-04-11 23:24:12 +0200

    ChatRoom

---
+++ src/components/ChatRoom.js
@@ -0,0 +1,86 @@
+import { useState, useEffect, useContext } from "react";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { AuthContext } from "../context/AuthContext";
+
+function ChatRoom({ recipientId }) {
+  const { currentUser } = useContext(AuthContext);
+  const [messages, setMessages] = useState([]);
+  const [messageText, setMessageText] = useState("");
+
+  useEffect(() => {
+    if (!recipientId) {
+      return;
+    }
+
+    const db = getFirestore();
+    const messagesRef = collection(db, "messages");
+    const q = query(
+      messagesRef,
+      where("senderId", "in", [getAuth().currentUser.uid, recipientId]),
+      where("recipientId", "in", [getAuth().currentUser.uid, recipientId])
+    );
+
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const messageList = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      messageList.orderBy()
+      setMessages(messageList);
+    });
+
+    return () => unsubscribe();
+  }, [recipientId]);
+
+  function sendMessage(event) {
+    event.preventDefault();
+
+    const db = getFirestore();
+    const messagesRef = collection(db, "messages");
+
+    addDoc(messagesRef, {
+      text: messageText,
+      senderId: getAuth().currentUser.uid,
+      recipientId: recipientId,
+    });
+
+    setMessageText("");
+  }
+
+  return (
+    <div>
+      <div>
+        {messages.map((message) => (
+          <div
+            key={message.id}
+            className={`message ${
+              message.senderId === currentUser.uid ? "sent" : "received"
+            }`}
+          >
+            <p>{message.text}</p>
+          </div>
+        ))}
+      </div>
+
+      <form onSubmit={sendMessage}>
+        <input
+          type="text"
+          value={messageText}
+          onChange={(event) => setMessageText(event.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+}
+
+export default ChatRoom;
---
+++ src/components/UsersList.js
@@ -0,0 +1,37 @@
+import { useState, useEffect, useContext } from "react";
+import { getFirestore, collection, onSnapshot } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { AuthContext } from "../context/AuthContext";
+
+function UsersList({ onSelectUser }) {
+  const { currentUser } = useContext(AuthContext);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const db = getFirestore();
+    const usersRef = collection(db, "users");
+
+    const unsubscribe = onSnapshot(usersRef, (snapshot) => {
+      const userList = snapshot.docs
+        .map((doc) => ({ id: doc.id, ...doc.data() }))
+        .filter((user) => user.id !== currentUser.uid); // filter out the current user
+      setUsers(userList);
+    });
+
+    return () => unsubscribe();
+  }, [currentUser.uid]);
+
+  return (
+    <div>
+      <ul>
+        {users.map((user) => (
+          <li className="user" key={user.id} onClick={() => onSelectUser(user.id)}>
+            {user.displayName}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default UsersList;
